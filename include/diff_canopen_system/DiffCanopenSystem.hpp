12813b4196a0e0f2fc64285f68b7cb2e259b9711
Xi Huang <sam.xihuang@gmail.com>
2023-05-03 17:53:01 +0200

    Fixed License

--- include/diff_canopen_system/DiffCanopenSystem.hpp
+++ include/diff_canopen_system/DiffCanopenSystem.hpp
@@ -1,5 +1,4 @@
-// Copyright (c) 2023, Xi Huang
-// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
